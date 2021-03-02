596dda76e90821cc6fd2e578d6ccbc967476abd3
Eli Elrom <elad.ny@gmail.com>
2021-03-02 23:19:15 +0300

    update to reflect changes to filenames generate-react-cli

---
+++ template/templates/page/TemplateName.test.tsx
@@ -0,0 +1,34 @@
+/*
+Author: Eli Elad Elrom
+Website: https://EliElrom.com
+License: MIT License
+Component: Component: src/pages/TemplateName/TemplateName.test.tsx
+*/
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import TemplateName from './TemplateName'
+
+const routeComponentPropsMock = {
+  history: {
+    location: {
+      pathname: '/TemplateName'
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  location: {} as any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  match: {} as any,
+}
+describe('<TemplateName />', () => {
+  let component
+
+  beforeEach(() => {
+    component = shallow(<TemplateName {...routeComponentPropsMock} />)
+  })
+
+  test('It should mount', () => {
+    expect(component.length).toBe(1)
+  })
+})
