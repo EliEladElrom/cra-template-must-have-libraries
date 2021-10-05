1ab4c6696a410bfa0ccf620e15b3d3b9bd804116
Eli Elrom <elad.ny@gmail.com>
2021-10-05 10:31:39 +0200

    bump jest puppeteer

--- template/e2e/app.test.tsx
+++ template/e2e/app.test.tsx
@@ -21,7 +21,7 @@ describe('<App />', () => {
   it('should include the React svg correct image', async () => {
     // @ts-ignore
     const images = await page.$$eval('img', anchors => [].map.call(anchors, img => img.src));
-    expect(images[0]).toMatch(`${SERVER_URL  }/static/media/logo.5d5d9eef.svg`)
+    expect(images[0]).toMatch(`${SERVER_URL  }/static/media/logo.103b5fa1.svg`)
   }, JEST_TIMEOUT)
 
 })
